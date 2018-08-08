3aaa9b96ee139ff2bf4c5f524664a4a3bc4f207d
renfushuai <renfushuai@xdf.cn>
2018-08-08 18:57:28 +0800

    封装RabbitMq

---
+++ XDF.Core/Helper/Log/LogHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace XDF.Core.Helper.Log
+{
+    public class LogHelper
+    {
+       private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public static void Debug(string txt)
+        {
+            Logger.Debug(txt);
+        }
+        public static void Info(string message)
+        {
+            Logger.Info(message);
+        }
+        public static void Error(string txt)
+        {
+            Logger.Error(txt);
+        }
+        public static void Sql(string sql)
+        {
+            SqlLogHelper.Info(sql);
+        }
+    }
+}
---
+++ XDF.Core/Helper/Log/SqlLogHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace XDF.Core.Helper.Log
+{
+   public class SqlLogHelper
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void Info(string message)
+        {
+            Logger.Info(message);
+        }
+    }
+}
